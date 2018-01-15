310485ff7334be48ef86f3568c8a3e5750b65b54
andikay <xtrmwar@gmx.at>
2018-01-15 17:28:47 +0100

    enable basic Fire Brigade functionality

--- src/app/layout/fahrzeuge/fahrzeuge.module.ts
+++ src/app/layout/fahrzeuge/fahrzeuge.module.ts
@@ -4,7 +4,7 @@ import { FahrzeugeRoutingModule } from './fahrzeuge-routing.module';
 import { FahrzeugeComponent } from './fahrzeuge.component';
 import { FahrzeugDetailComponent } from './fahrzeug-detail/fahrzeug-detail.component';
 import { FahrzeugEditComponent } from './fahrzeug-edit/fahrzeug-edit.component';
-import {FormsModule} from "@angular/forms";
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
     imports: [CommonModule, FahrzeugeRoutingModule, FormsModule],
